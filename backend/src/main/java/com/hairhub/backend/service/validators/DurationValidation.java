06126ae8249a66f438507bdb3fca990fe5b9efa7
razvanexu <razvandf@gmail.com>
2026-07-01 14:47:23 +0300

    invalidare timp negativ programare, adaugare teste la validari

--- backend/src/main/java/com/hairhub/backend/service/validators/DurationValidation.java
+++ backend/src/main/java/com/hairhub/backend/service/validators/DurationValidation.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class DurationValidation {
     public void validate(Integer duration){
-        if(duration == null || duration %30 != 0){
+        if(duration == null || duration <= 0 || duration %30 != 0){
             throw new IllegalArgumentException("Duration must be a multiple of 30 minutes");
         }
     }
