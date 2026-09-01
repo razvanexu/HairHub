c7b8fa8417493e5aa85403d219d3b9d693dfa1e1
razvanexu <razvandf@gmail.com>
2026-09-01 21:41:09 +0300

    CORS configuration added

---
+++ backend/src/main/java/com/hairhub/backend/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.hairhub.backend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Value("${app.cors.allowed-origin}")
+    String allowedOrigin;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigin)
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowedHeaders("*");
+    }
+}
